01aba10d7770e8091011983e869f18549fb84ca3
Coimbra <adsjkbd@gmail.com>
2025-03-01 08:11:02 -0300

    adicionando documentacao swagger

--- src/main/java/com/jkbd/transacao_api/controller/TransacaoController.java
+++ src/main/java/com/jkbd/transacao_api/controller/TransacaoController.java
@@ -2,6 +2,9 @@ package com.jkbd.transacao_api.controller;
 
 import com.jkbd.transacao_api.business.services.TransacaoService;
 import com.jkbd.transacao_api.controller.dtos.TransacaoRequestDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,16 @@ public class TransacaoController {
     private final TransacaoService transacaoService;
 
     @PostMapping
+    @Operation(
+            summary = "Adicionar uma transação",
+            description = "Endpoint responsável por adicionar transações"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Transação gravada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Campos não atendem os requisitos da transação"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<Void> adicionarTransacao(@RequestBody TransacaoRequestDTO dto) {
         transacaoService.adicionarTrasacoes(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
@@ -22,6 +35,15 @@ public class TransacaoController {
     }
 
     @DeleteMapping
+    @Operation(
+            summary = "Adicionar uma transação",
+            description = "Endpoint responsável por deletar transações"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Transação deletadas com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<Void> deletarTransacoes() {
         transacaoService.limparTransacoes();
         return ResponseEntity.ok().build();
