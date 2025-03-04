b07cc3b66c215910d1d6bb035f547c9354fcbbd4
Coimbra <adsjkbd@gmail.com>
2025-03-04 19:50:22 -0300

    adicionando actuator

--- src/main/java/com/jkbd/transacao_api/controller/GlobalExceptionHandler.java
+++ src/main/java/com/jkbd/transacao_api/controller/GlobalExceptionHandler.java
@@ -1,14 +1,20 @@
 package com.jkbd.transacao_api.controller;
 
 import com.jkbd.transacao_api.infrastructure.exceptions.UnprocessableEntity;
+import io.swagger.v3.oas.annotations.Hidden;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(UnprocessableEntity.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
@@ -18,7 +24,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<String> handlerUnprocessableEntity(Exception e) {
+    public ResponseEntity<String> handlerGenericException(Exception e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " + e.getMessage());
     }
+
 }
